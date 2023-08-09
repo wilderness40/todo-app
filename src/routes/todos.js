6efa3db2f64cbe499ccad9cdd099328f7ba6e6d9
seunghyun <inyo0506@gmail.com>
2023-08-09 15:06:09 +0900

    08.09 final

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -5,18 +5,45 @@ const { isAuth } = require('../../auth')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {  // /api/todos/
-    res.json('전체 할일목록 조회')
-}) 
+router.get('/', isAuth, expressAsyncHandler(async(req, res, next) => {
+    const todos = await Todo.find({ author : req.user._id }) // 해당 사용자의 할일목록 조회
+    if(todos.length === 0){
+        res.status(404).json({ code: 404, message: 'Failed to find todos'})
+    }else{
+        res.json({code: 200, todos}) // todos : todos: todos를 기재
+    }
+})) 
 
 router.get('/:id', (req, res, next) => {  // /api/todos/{id}
     res.json('특정 할일 조회')
 })
 
-router.post('/', (req, res, next) => {  // /api/todos/
-    res.json('새로운 할일 생성')
-})
-
+router.post('/', isAuth, expressAsyncHandler(async(req, res, next) => {
+    // 중복체크 (현재 사용자가 생성하려는 TODO의 타이틀이 이미 DB에 있는지 먼저 검사해야함)
+    const searchedTodo = await Todo.findOne({
+        author: req.user._id,
+        title: req.body.title,
+    })
+    if(searchedTodo){
+        res.status(204).json({ code:204, message: '이미 todo 있음'})
+    }else{
+        const todo = new Todo({
+            author: req.user._id, // 현재 사용자의 id
+            title: req.body.title,
+            descripttion: req.body.descripttion
+        })
+        const newTodo = await todo.save()
+        if(!newTodo){
+            res.status(401).json({ code: 401, message: 'Failed to save todo'})
+        }else{
+            res.status(201).json({
+                code:201,
+                message: 'New Todo Created',
+                newTodo
+            })
+        }
+    }
+}))
 router.put('/:id', (req, res, next) => { // /api/todos/{id}
     res.json('특정 할일 변경')
 })
